e3ba1c6a8cc3caeb434e36d29de6ad8e1f736d4b
jfranko1399 <jfranko13@icloud.com>
2022-03-20 17:37:29 -0400

    working on step 4

--- Lab 5/triangle.c
+++ Lab 5/triangle.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define VLEN 15
 #define FLEN 13
@@ -12,7 +13,9 @@ int main(int argc, char *argv[])
   FILE *fpt2;
   char *str, *buff;
   int maxVert, maxFace, len, diff, i, j;
-  double **vertices, *tempV;
+  double **vertices, *tempV, max[3], min[3];
+  double xCenter, yCenter, zCenter, E, xRad, yRad, zRad;
+  double Rx[3][3], Ry[3][3], Rz[3][3];
   int **faces, *tempF;
 
   if (argc != 5)
@@ -29,6 +32,10 @@ int main(int argc, char *argv[])
 
   str = (char *)calloc(100, sizeof(char));
   buff = (char *)calloc(10, sizeof(char));
+
+  ///////////////////////////////
+  /// # of Vertices and Faces ///
+  ///////////////////////////////
 
   fgets(str, 100, fpt);
   fgets(str, 100, fpt);
@@ -54,8 +61,18 @@ int main(int argc, char *argv[])
   maxFace = atoi(buff);
   //printf("%d\n", faces);
 
+  ///////////////////////////////
+  /// Read Vertices and Faces ///
+  ///////////////////////////////
+
   vertices = (double **)calloc(maxVert, sizeof(double *));
   faces = (int **)calloc(maxFace, sizeof(int *));
+
+  for (i = 0; i < 3; i++)
+  {
+    max[i] = 0;
+    min[i] = 1000000;
+  }
 
   fgets(str, 100, fpt);
   fgets(str, 100, fpt);
@@ -72,7 +89,42 @@ int main(int argc, char *argv[])
       vertices[i][j] = strtod(strtok(NULL, " "), NULL);
       //printf("%f ", vertices[i][j]);
     }
+
+    for (j = 0; j < 3; j++)
+    {
+      if (vertices[i][j] > max[j])
+      {
+        max[j] = vertices[i][j];
+      }
+      if (vertices[i][j] < min[j])
+      {
+        min[j] = vertices[i][j];
+      }
+    }
     //printf("\n");
+  }
+
+  // for (i = 0; i < 3; i++)
+  // {
+  //   printf("%f ", max[i]);
+  // }
+  // printf("\n");
+  // for (i = 0; i < 3; i++)
+  // {
+  //   printf("%f ", min[i]);
+  // }
+  // printf("\n");
+
+  xCenter = (max[0] + min[0])/2;
+  yCenter = (max[1] + min[1])/2;
+  zCenter = (max[2] + min[2])/2;
+  E = -1000000;
+  for (i = 0; i < 3; i++)
+  {
+    if ((max[i] - min[i]) > E)
+    {
+      E = (max[i] - min[i]);
+    }
   }
 
   for (i = 0; i < maxFace; i++)
@@ -89,4 +141,18 @@ int main(int argc, char *argv[])
     }
     //printf("\n");
   }
+
+  ////////////////////////////
+  /// Calc Camera Position ///
+  ////////////////////////////
+
+  xRad = strtod(argv[2], NULL) * (M_PI/180);
+  yRad = strtod(argv[3], NULL) * (M_PI/180);
+  zRad = strtod(argv[4], NULL) * (M_PI/180);
+
+  Rx = {{1, 0, 0}, {0, cos(xRad), -sin(xRad)}, {0, sin(xRad), cos(xRad)}};
+  Ry = {{cos(yRad), 0, sin(yRad)}, {0, 1, 0}, {-sin(yRad), 0, cos(yRad)}};
+  Rz = {{cos(zRad), -sin(zRad), 0}, {sin(zRad), cos(zRad, 0)}, {0, 0, 1}};
+
+  
 }

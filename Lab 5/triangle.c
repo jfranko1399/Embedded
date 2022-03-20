00d8029c17cdf78f2bc1283740556e5c43780e9f
jfranko1399 <jfranko13@icloud.com>
2022-03-20 18:47:55 -0400

    step 5 finished

--- Lab 5/triangle.c
+++ Lab 5/triangle.c
@@ -7,6 +7,75 @@
 #define VLEN 15
 #define FLEN 13
 
+double *scalarMult(int scalar, double *array);
+double dotProd(double *A, double *B);
+double *crossProd(double *A, double *B);
+double *vectorAdd(double *A, double *B);
+double *vectorSub(double *A, double *B);
+
+double *scalarMult(int scalar, double *array)
+{
+  double *result = (double *)calloc(3, sizeof(double));
+  int i;
+
+  for (i = 0; i < 3; i++)
+  {
+    result[i] = scalar * array[i];
+  }
+
+  return result;
+}
+
+double dotProd(double *A, double *B)
+{
+  double result = 0.0;
+  int i;
+
+  for (i = 0; i < 3; i++)
+  {
+    result += A[i] * B[i];
+  }
+
+  return result;
+}
+
+double *crossProd(double *A, double *B)
+{
+  double *result = (double *)calloc(3, sizeof(double));
+
+  result[0] = (A[1] * B[2]) - (A[2] * B[1]);
+  result[1] = (A[2] * B[0]) - (A[0] * B[2]);
+  result[2] = (A[0] * B[1]) - (A[1] * B[0]);
+
+  return result;
+}
+
+double *vectorAdd(double *A, double *B)
+{
+  double *result = (double *)calloc(3, sizeof(double));
+  int i;
+
+  for (i = 0; i < 3; i++)
+  {
+    result[i] = A[i] + B[i];
+  }
+
+  return result;
+}
+
+double *vectorSub(double *A, double *B)
+{
+  double *result = (double *)calloc(3, sizeof(double));
+  int i;
+
+  for (i = 0; i < 3; i++)
+  {
+    result[i] = A[i] - B[i];
+  }
+
+  return result;
+}
+
 int main(int argc, char *argv[])
 {
   FILE *fpt;
@@ -14,8 +83,7 @@ int main(int argc, char *argv[])
   char *str, *buff;
   int maxVert, maxFace, len, diff, i, j;
   double **vertices, *tempV, max[3], min[3];
-  double xCenter, yCenter, zCenter, E, xRad, yRad, zRad;
-  double Rx[3][3], Ry[3][3], Rz[3][3];
+  double center[3], E, xRad, yRad, zRad, *temp, a;
   int **faces, *tempF;
 
   if (argc != 5)
@@ -115,9 +183,9 @@ int main(int argc, char *argv[])
   // }
   // printf("\n");
 
-  xCenter = (max[0] + min[0])/2;
-  yCenter = (max[1] + min[1])/2;
-  zCenter = (max[2] + min[2])/2;
+  center[0] = (max[0] + min[0])/2;
+  center[1] = (max[1] + min[1])/2;
+  center[2] = (max[2] + min[2])/2;
   E = -1000000;
   for (i = 0; i < 3; i++)
   {
@@ -150,9 +218,46 @@ int main(int argc, char *argv[])
   yRad = strtod(argv[3], NULL) * (M_PI/180);
   zRad = strtod(argv[4], NULL) * (M_PI/180);
 
-  Rx = {{1, 0, 0}, {0, cos(xRad), -sin(xRad)}, {0, sin(xRad), cos(xRad)}};
-  Ry = {{cos(yRad), 0, sin(yRad)}, {0, 1, 0}, {-sin(yRad), 0, cos(yRad)}};
-  Rz = {{cos(zRad), -sin(zRad), 0}, {sin(zRad), cos(zRad, 0)}, {0, 0, 1}};
+  double Rx[3][3] = {{1, 0, 0}, {0, cos(xRad), -sin(xRad)}, {0, sin(xRad), cos(xRad)}};
+  double Ry[3][3] = {{cos(yRad), 0, sin(yRad)}, {0, 1, 0}, {-sin(yRad), 0, cos(yRad)}};
+  double Rz[3][3] = {{cos(zRad), -sin(zRad), 0}, {sin(zRad), cos(zRad), 0}, {0, 0, 1}};
 
-  
+  double camera[3] = {1, 0, 0};
+  double up[3] = {0, 0, 1};
+  temp = scalarMult(1.5 * E, camera);
+  for (i = 0; i < 3; i++)
+  {
+    camera[i] = temp[i] + center[i];
+  }
+
+  ////////////////////////////////////
+  temp = vectorSub(center, camera);
+  double* left = crossProd(up, temp);
+  ////////////////////////////////////
+  a = 0;
+  for (i = 0; i < 3; i++)
+  {
+    a += left[i] * left[i];
+  }
+  a = sqrt(a);
+  ////////////////////////////////////
+  temp = scalarMult(E/(2 * a), left);
+  left = vectorAdd(temp, center);
+  ////////////////////////////////////
+  temp = vectorSub(center, camera);
+  double *right = crossProd(temp, up);
+  ////////////////////////////////////
+  temp = scalarMult(E/(2 * a), right);
+  right = vectorAdd(temp, center);
+  ////////////////////////////////////
+  temp = scalarMult(E/2, up);
+  double *top = vectorAdd(temp, center);
+  ////////////////////////////////////
+  temp = scalarMult(-E/2, up);
+  double *bottom = vectorAdd(temp, center);
+  ////////////////////////////////////
+  temp = scalarMult(E/2, up);
+  double *topLeft = vectorAdd(temp, left);
+
+
 }
